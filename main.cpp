4f949a46d1e04a86afa96a44134cdb877dd83c6a
segevbr <segevbr@gmail.com>
2024-10-10 14:14:56 +0300

    Bug fixes

--- main.cpp
+++ main.cpp
@@ -5,9 +5,9 @@
 #include <iostream>
 
 // Sphere intersection, returns true if there is 1 or two intersections.
-bool hit_sphere(const point3& center, double radius, cons ray& r){
+bool hit_sphere(const point3& center, double radius, const ray& r){
     vec3 oc = center - r.origin();
-    auto a = dot(r.direction(), r.direction);
+    auto a = dot(r.direction(), r.direction());
     auto b = -2.0 * dot(r.direction(), oc);
     auto c = dot(oc, oc) - (radius*radius);
     auto discriminant = b*b - 4*a*c;
@@ -16,7 +16,7 @@ bool hit_sphere(const point3& center, double radius, cons ray& r){
 
 // Determine ray color
 color ray_color(const ray& r){
-    if (hit_sphere(point(0,0,-1), 0.5, r)) return color(1, 0, 0);
+    if (hit_sphere(point3(0,0,-1), 0.5, r)) return color(1, 0, 0);
     
     vec3 unit_direction = unit_vector(r.direction());
     auto a = 0.5*(unit_direction.y() + 1.0);
