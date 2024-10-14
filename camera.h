dc551284a325ed1958e75294b52008e4a92562ff
segevbr <segevbr@gmail.com>
2024-10-14 15:21:16 +0300

    Added antialiasing method of finding "average" color around a pixel

--- camera.h
+++ camera.h
@@ -4,7 +4,8 @@
 #include "hittable.h"
 
 class camera {
-    int    image_height;   // Rendered image height
+    int image_height;   // Rendered image height
+    int pixel_samples_scale;
     point3 center;         // Camera center
     point3 pixel00_loc;    // Location of pixel 0, 0
     vec3   pixel_delta_u;  // Offset to pixel to the right
@@ -13,6 +14,8 @@ class camera {
     void initialize() {
         image_height = int(image_width / aspect_ratio);
         image_height = (image_height < 1) ? 1 : image_height;
+
+        pixel_samples_scale = 1.0 / samples_per_pixels;
 
         center = point3(0, 0, 0);
 
@@ -35,6 +38,26 @@ class camera {
         pixel00_loc = viewport_upper_left + 0.5 * (pixel_delta_u + pixel_delta_v);
     }
 
+    ray get_ray(int i, int j) const {
+        // Construct a cmaera ray originating from the origin and directed at
+        // randomaly sampled point around the pixel location (i, j).
+
+        auto offset = sample_square();
+        auto pixel_sample = pixel00_loc
+                          + ((i + offset.x()) * pixel_delta_u)
+                          + ((j + offset.y()) * pixel_delta_v);
+
+        auto ray_origin = center;
+        auto ray_direction = pixel_sample - ray_origin;
+
+        return ray(ray_origin, ray_direction);
+    }
+
+    vec3 sample_square() const {
+        // Return the vector to a random point in the [-.5,-.5]-[+.5,+.5] unit square
+        return vec3(random_double() - 0.5, random_double() - 0.5, 0)
+    }
+
     color ray_color(const ray& r, const hittable& world) const {
         hit_record rec;
 
@@ -49,7 +72,8 @@ class camera {
 
     public:
         double aspect_ratio = 1.0;  // Ratio of image width over height
-        int    image_width  = 100;  // Rendered image width in pixel count
+        int image_width  = 100;     // Rendered image width in pixel count
+        int samples_per_pixels = 10; // Count of random samples for each pixel
 
         void render(const hittable& world) {
             initialize();
@@ -60,13 +84,13 @@ class camera {
                 std::clog << "\rScanlines remaining: " << (image_height - j) << ' ' 
                 << std::flush;
                 for (int i = 0; i < image_width; i++) {
-                    auto pixel_center = pixel00_loc + (i * pixel_delta_u) 
-                    + (j * pixel_delta_v);
-                    auto ray_direction = pixel_center - center;
-                    ray r(center, ray_direction);
+                    color pixel_color(0, 0, 0);
 
-                    color pixel_color = ray_color(r, world);
-                    write_color(std::cout, pixel_color);
+                    for (int sample = 0; sample < samples_per_pixels; sample++){
+                        ray r = get_ray(i,j);
+                        pixel_color += ray_color(r, world);    
+                    }
+                    write_color(std::cout, pixel_samples_scale * pixel_color);
                 }
             }
 
